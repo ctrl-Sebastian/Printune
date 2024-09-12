bfc48211edff61321f55e62fabdc2d76fa82982c
Sebastian De Leon <sebastianipl0131@gmail.com>
2024-09-12 15:48:46 -0400

    test1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include<iostream>
+#include "vec3.h"
+#include "triangle.h"
+#include "mesh.h"
 using namespace std;
 
 int main()
 {
+    Mesh mesh1;
+    mesh1.add(Vec3(0,0,0), Vec3(0,0,1), Vec3(0,1,0));
 
-
+    mesh1.stl_write("test.stl");
     return 0;
 }
