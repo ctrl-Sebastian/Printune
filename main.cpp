91a3767c40ec2526fdd39a388d6891ebb6f58edc
Sebastian De Leon <sebastianipl0131@gmail.com>
2024-09-12 16:24:22 -0400

    cleaning

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
-#include "vec3.h"
-#include "triangle.h"
-#include "mesh.h"
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    Mesh mesh1;
-    mesh1.add(Vec3(0,0,0), Vec3(0,0,1), Vec3(0,1,0));
 
-    mesh1.stl_write("test.stl");
     return 0;
 }
